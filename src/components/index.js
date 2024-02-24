eb9724bcc3e595093fc648579fbefbcba3df1387
Arghya <arghyadas242004@gmail.com>
2024-02-24 13:50:00 +0530

    Shifting to NextJS

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from './Navbar/Navbar'
-export { default as Header } from './Header/Header'
-export { default as About } from './About/About'
-export { default as Projects } from './Projects/Projects'
-export { default as Skills } from './Skills/Skills'
-export { default as Footer } from './Footer/Footer'
-export { default as Experience } from "./Experience/Experience"
