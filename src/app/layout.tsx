acdce48e105ab6e224882039887352b53e42fbc8
Arghya <arghyadas242004@gmail.com>
2024-07-14 01:24:56 +0530

    fix: navar header overflay on content click

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-pt-20 sm:scroll-pt-0">
       <body className={inter.className}>{children}</body>
     </html>
   );
